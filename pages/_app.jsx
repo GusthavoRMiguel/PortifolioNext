97d5c00aaf59412f071fa9d9cf7116674bfd621d
GusthavoRMiguel <108017738+GusthavoRMiguel@users.noreply.github.com>
2023-03-14 17:30:23 -0300

    analytics_att

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css'
 import Head from "next/head";
+import  { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
